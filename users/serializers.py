ddfe11b36980cce0157df0b084ae72528c7651b8
georgegyan <georgegyankwame72@gmail.com>
2026-04-11 20:20:24 +0000

    Add role selection (is_gym_owner) to registration flow

--- users/serializers.py
+++ users/serializers.py
@@ -24,11 +24,12 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     
     password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
     password2 = serializers.CharField(write_only=True, required=True)
+    is_gym_owner = serializers.BooleanField(default=False, required=False)  
     
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'password2', 'first_name', 
-                  'last_name', 'phone_number']
+                  'last_name', 'phone_number', 'is_gym_owner']
     
     def validate(self, attrs):
         """
@@ -48,7 +49,11 @@ class UserRegisterSerializer(serializers.ModelSerializer):
         # Create user with hashed password
         user = User.objects.create_user(**validated_data)
         return user
-
+    
+    def validate_is_gym_owner(self, value):
+        if not isinstance(value, bool):
+            raise serializers.ValidationError("is_gym_owner must be a boolean value.")
+        return value
 
 class UserDetailSerializer(serializers.ModelSerializer):
     """
@@ -62,3 +67,4 @@ class UserDetailSerializer(serializers.ModelSerializer):
                   'phone_number', 'is_gym_owner', 'profile_picture', 
                   'date_joined', 'last_login']
         read_only_fields = ['id', 'is_gym_owner', 'date_joined', 'last_login']
+
