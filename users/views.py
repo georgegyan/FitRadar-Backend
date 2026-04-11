dad42b924f308abd04c316835ff8840345f7cba3
georgegyan <georgegyankwame72@gmail.com>
2026-04-11 00:08:45 +0000

    Add API views and endpoints for authentication, gyms, and bookings with JWT

--- users/views.py
+++ users/views.py
@@ -1,4 +1,7 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
 from django.contrib.auth import get_user_model
 from .serializers import UserRegisterSerializer, UserSerializer, UserDetailSerializer
 
@@ -9,10 +12,18 @@ class RegisterView(generics.CreateAPIView):
     permission_classes = [permissions.AllowAny]
     serializer_class = UserRegisterSerializer
 
-
 class ProfileView(generics.RetrieveUpdateAPIView):
     serializer_class = UserDetailSerializer
     permission_classes = [permissions.IsAuthenticated]
     
     def get_object(self):
         return self.request.user
+
+class CustomTokenObtainPairView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+        if response.status_code == 200:
+            user = User.objects.get(username=request.data.get('username'))
+            user_serializer = UserSerializer(user)
+            response.data['user'] = user_serializer.data
+        return response
