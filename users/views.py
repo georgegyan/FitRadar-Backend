bd973929ce3462acf5523e99af8cd411541a88c2
georgegyan <georgegyankwame72@gmail.com>
2026-04-17 23:14:10 +0000

    Add user registration endpoint with is_gym_owner field

--- users/views.py
+++ users/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializers import UserRegistrationSerializer
 
-# Create your views here.
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+
+        return Response({
+            "message": "User registered successfully",
+            "username": user.username,
+            "is_gym_owner": user.is_gym_owner
+        }, status=status.HTTP_201_CREATED)
