4bbf35f6b732ae1e91b15a5747bc5114f228c2af
georgegyan <georgegyankwame72@gmail.com>
2026-04-17 22:47:44 +0000

    Clean restart: add custom user model with is_gym_owner

--- bookings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import UserBookingsView, CreateBookingView, CancelBookingView, BookingDetailView
-
-urlpatterns = [
-    path('', UserBookingsView.as_view(), name='user-bookings'),
-    path('create/', CreateBookingView.as_view(), name='create-booking'),
-    path('<int:pk>/', BookingDetailView.as_view(), name='booking-detail'),
-    path('<int:pk>/cancel/', CancelBookingView.as_view(), name='cancel-booking'),
-]
--- users/views.py
+++ users/views.py
@@ -1,29 +1,3 @@
-from rest_framework import generics, permissions, status
-from rest_framework.response import Response
-from rest_framework.views import APIView
-from rest_framework_simplejwt.views import TokenObtainPairView
-from django.contrib.auth import get_user_model
-from .serializers import UserRegisterSerializer, UserSerializer, UserDetailSerializer
+from django.shortcuts import render
 
-User = get_user_model()
-
-class RegisterView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    permission_classes = [permissions.AllowAny]
-    serializer_class = UserRegisterSerializer
-
-class ProfileView(generics.RetrieveUpdateAPIView):
-    serializer_class = UserDetailSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    
-    def get_object(self):
-        return self.request.user
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    def post(self, request, *args, **kwargs):
-        response = super().post(request, *args, **kwargs)
-        if response.status_code == 200:
-            user = User.objects.get(username=request.data.get('username'))
-            user_serializer = UserSerializer(user)
-            response.data['user'] = user_serializer.data
-        return response
+# Create your views here.
